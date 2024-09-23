2d617bbd32c7d038d67e44c05d0139e31b23f582
anemonemon <monika.skink@gmail.com>
2024-09-23 19:50:56 +0300

    Initial commit

---
+++ main2.py
@@ -0,0 +1,142 @@
+# print('skaiciai 2')
+# print('2 uzduotis')
+# print()
+# print('iveskite pirma skaiciu:')
+# sk1 = int(input())
+# print('iveskite antra skaiciu:')
+# sk2 = int(input())
+# sk3 = sk1 + sk2
+# print(sk1,'+',sk2,'=', sk3)
+
+# print('3 uzduotis')
+# print()
+# print('iveskite pirma skaiciu:')
+# sk1 = int(input())
+# print('iveskite antra skaiciu:')
+# sk2 = int(input())
+# print('iveskite trecia skaiciu:')
+# sk3 = int(input())
+# print(sk1,'+',sk2, '+', sk3, '=', sk1 + sk2 + sk3)
+# print(sk1,'-',sk2, '-', sk3, '=', sk1 - sk2 - sk3)
+# print(sk1,'*',sk2, '*', sk3, '=', sk1 * sk2 * sk3)
+# print(sk1,'/',sk2, '/', sk3, '=', sk1 / sk2 / sk3)
+
+# print('4 uzduotis')
+# print()
+# print('iveskite pirma skaiciu:')
+# sk1 = int(input())
+# print('iveskite antra skaiciu:')
+# sk2 = int(input())
+# print(sk1,'padidintas 5-iais yra', sk1 + 5)
+# print(sk2, 'padidintas 2 kartus yra', sk2 * 2)
+
+# print('info ivedimas')
+# print('2 uzduotis')
+# print()
+# print('iveskite bet koki simboli:')
+# kint = input()
+# print(kint,kint,kint)
+# print(kint, ' ', kint)
+# print(kint,kint,kint)
+
+# print('3 uzduotis')
+# print()
+# print('iveskite bet koki simboli:')
+# kint2 = input()
+# print(kint2)
+# print(kint2, kint2)
+# print(kint2,kint2,kint2)
+
+# print('4 uzduotis')
+# print()
+# print('Iveskite savo varda:')
+# vardas = input()
+# print('Iveskite savo amziu:')
+# amzius = int(input())
+# print('Iveskite savo ugi (metrais):')
+# ugis = float(input())
+# print('Ivesta informacija')
+# print(vardas, type(vardas))
+# print(amzius, type(amzius))
+# print(ugis, type(ugis))
+
+# print('5 uzduotis')
+# print()
+# print('Iveskite pirma pazymi:')
+# pirm = int(input())
+# print('Iveskite antra pazymi:')
+# antr = int(input())
+# print('Iveskite trecia pazymi:')
+# trec = int(input())
+# print('Iveskite ketvirta pazymi:')
+# ketvir = int(input())
+# print('Iveskite penkta pazymi:')
+# penkt = int(input())
+# print('Ivestu pazymiu vidurkis yra:', (pirm + antr + trec + ketvir + penkt)/5)
+
+# print('6 uzduotis')
+# print()
+# print('Iveskite metrus:')
+# metrai = int(input())
+# cent = metrai * 100
+# mili = metrai * 1000
+# kil = metrai / 1000
+# print('Jusu ivestas skaicius paverstas centimentrais yra:', cent, 'cm')
+# print('Jusu ivestas skaicius paverstas milimentrais yra:', mili, 'mm')
+# print('Jusu ivestas skaicius paverstas kilomentrais yra:', kil, 'km')
+
+# print('7 uzduotis')
+# print()
+# print('iveskite pirma skaiciu:')
+# sk1 = int(input())
+# print('iveskite antra skaiciu:')
+# sk2 = int(input())
+# print(sk1,'+',sk2, '=', sk1 + sk2)
+# print(sk1,'-',sk2, '=', sk1 - sk2)
+# print(sk1,'*',sk2, '=', sk1 * sk2)
+# print(sk1,'/',sk2, '=', sk1 / sk2)
+
+# print('8 uzduotis')
+# print()
+# print('iveskite pirma skaiciu:')
+# sk1 = int(input())
+# print('iveskite antra skaiciu:')
+# sk2 = int(input())
+# print('Liekana padalinus pirma skaiciu is antro yra:', sk1%sk2)
+
+# print('9 uzduotis')
+# print()
+# print('iveskite pirma skaiciu:')
+# sk1 = int(input())
+# print('iveskite antra skaiciu:')
+# sk2 = int(input())
+# print(f'{sk1} pakelta {sk2}-uoju laipsniu yra: {sk1**sk2}')
+
+
+print('iveskite pirma skaiciu:')
+sk1 = int(input())
+print('iveskite antra skaiciu:')
+sk2 = int(input())
+print('iveskite trecia skaiciu:')
+sk3 = int(input())
+
+if sk1 == sk2:
+    print('pirmas ir antras skaiciai yra lygus')
+if sk2 == sk3:
+    print('antras ir trecias skaiciai yra lygus')
+if sk1 > sk2:
+    print('pirmas skaicius yra didesnis uz antraji')
+if sk2 > sk3 * 2:
+    print('antras skaicius yra didesnis uz trecia skaiciu padauginta is 2')
+if sk1 % 2 == 0:
+    print('pirmas skaicius yra lyginis')
+if sk2 % 2 == 1:
+    print('antras skaicius yra nelyginis')
+if sk3 > 0:
+    print('trecias skaicius yra teigiamas')
+if sk1 < 0:
+    print('pirmas skaicius yra neigiamas')
+if sk2 % 4 == 0:
+    print('antras skaicius dalinasi is 4')
+if sk3 % 8 == 0:
+    print('trecias skaicius dalinasi is 8')
